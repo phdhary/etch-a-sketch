8f87ea2571438326a9eca9559184e273d8d9876c
Hary Prihadi <phdhary@gmail.com>
2023-08-30 22:32:06 +0700

    feat: create a function for grid creation

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const container = document.querySelector("div.container");
 
-const size = 16;
-const total = size * size + size;
-for (let i = 1; i < total; i++) {
-  const div = document.createElement("div");
-  div.addEventListener("mouseover", (ev) => {
-    ev.stopPropagation();
-    div.classList.add("hover");
-  });
-  container.appendChild(div);
+function createGrid(size) {
+  container.innerHTML = "";
+  const total = size * size + size;
+  for (let i = 1; i < total; i++) {
+    const div = document.createElement("div");
+    div.addEventListener("mouseover", () => {
+      div.classList.add("hover");
+    });
+    container.appendChild(div);
+  }
 }
+
+createGrid(16);
